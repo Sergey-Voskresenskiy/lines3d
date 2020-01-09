5ff047ada686e5e0bbd1fb171aeba812546a55c8
Sergey <imsheenitstrue@gmail.com>
2020-01-09 17:00:57 +0200

    finish?

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ renderer.setClearColor(0x333333);
 // camera
 
 let camera = new THREE.PerspectiveCamera(40, width / height, 1, 1000);
-camera.position.set(-100, 100, 300);
+camera.position.set(0, 0, 600);
 
 // camera END
 
@@ -46,47 +46,78 @@ let material = new THREE.LineBasicMaterial({
   color: 0x848484
 });
 
-for (let i = 0; i < lines; i++) {
+// for (let i = 0; i < lines; i++) {
+//   let geometry = new THREE.Geometry();
+//   let line = new THREE.Line(geometry, material);
+
+//   for (let j = 0; j < dots; j++) {
+
+//     let coord = (j / dots) * radius * 2 - radius;
+
+//     let vector = new THREE.Vector3(coord, 0)
+//     geometry.vertices.push(vector);
+
+//   }
+
+//   line.rotation.x = Math.random() * Math.PI
+//   line.rotation.y = Math.random() * Math.PI
+//   line.rotation.z = Math.random() * Math.PI
+
+//   group.add(line)
+// }
+
+// function UpdateLines(time) {
+//   let line, vector, ratio
+//   for (let i = 0; i < lines; i++) {
+//     line = group.children[i];
+//     for (let j = 0; j < dots; j++) {
+//       vector = line.geometry.vertices[j];
+//       ratio = 1 - (radius - Math.abs(vector.x))/radius;
+//       vector.y = Math.sin(j/5 + time/100)* 20*ratio;
+//     }
+//     line.geometry.verticesNeedUpdate = true;
+//   }
+// }
+
+// line END
+
+
+
+let canvas2d = document.querySelector('#canvas2')
+let ctx = canvas2d.getContext('2d')
+let size = 200;
+
+canvas2d.width = size;
+canvas2d.height = size;
+
+let img = document.querySelector('#img')
+
+ctx.drawImage(img, 0, 0, size, size)
+
+let data = ctx.getImageData(0, 0, size, size);
+data = data.data;
+
+for (let y = 0; y < size; y++) {
+
   let geometry = new THREE.Geometry();
   let line = new THREE.Line(geometry, material);
 
-  for (let j = 0; j < dots; j++) {
-
-    let coord = (j / dots) * radius * 2 - radius;
-
-    let vector = new THREE.Vector3(coord, 0)
+  for (let x = 0; x < size; x++) {
+    let bright = data[((size * y) + x) * 4];
+    let vector = new THREE.Vector3(x - 100, y - 100, bright)
     geometry.vertices.push(vector);
-
   }
-
-  line.rotation.x = Math.random() * Math.PI
-  line.rotation.y = Math.random() * Math.PI
-  line.rotation.z = Math.random() * Math.PI
-
+  line.rotation.z = 3.2
   group.add(line)
-}
 
-function UpdateLines(time) {
-  let line, vector, ratio
-  for (let i = 0; i < lines; i++) {
-    line = group.children[i];
-    for (let j = 0; j < dots; j++) {
-      vector = line.geometry.vertices[j];
-      ratio = 1 - (radius - Math.abs(vector.x))/radius;
-      vector.y = Math.sin(j/5 + time/100)* 20*ratio;
-    }
-    line.geometry.verticesNeedUpdate = true;
-  }
 }
-
-// line END
 
 let time = 0;
 
 function Render() {
   time++;
   renderer.render(scene, camera);
-  UpdateLines(time);
+  // UpdateLines(time);
   window.requestAnimationFrame(Render);
 }
 
